fa09faa7d36654b6e09754c09c8dd1cab2163159
Daniel Kongsgaard <daniel@Daniels-MBP.webspeed.dk>
2023-08-05 01:59:33 +0200

    Added catppuccin as a theme option.

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -10,9 +10,32 @@ require("onedark").setup({
 })
 require('onedark').load()
 
+require("catppuccin").setup({
+  flavour = "mocha",
+  transparent_background = true,
+  integrations = {
+    neogit = true,
+    which_key = true,
+    lsp_trouble = true,
+    gitsigns = true,
+    notify = true,
+  }
+})
+
+
 vim.keymap.set('n', '<leader>b', function()
-  local transp = vim.g.onedark_config.transparent
-  require('onedark').setup({ transparent = not transp })
-  require('onedark').load()
+  local cur_theme = vim.g.colors_name
+  local transp = true
+  if cur_theme == "onedark" then
+    transp = vim.g.onedark_config.transparent
+    require('onedark').setup({ transparent = not transp })
+    require('onedark').load()
+  elseif cur_theme == "catppuccin-mocha" then
+    transp = require("catppuccin").options.transparent_background
+    require("catppuccin").setup({ transparent_background = not transp })
+    require("catppuccin").load()
+  else
+    print("Error: Not onedark or catppuccin-mocha.")
+  end
 end, { desc = '[B]ackground Transparent Toggle' })
 
--- nvim/after/plugin/zen.lua
+++ nvim/after/plugin/zen.lua
@@ -2,8 +2,18 @@
 
 vim.keymap.set('n', '<leader>z', function()
   require('zen-mode').toggle()
-  local transp = vim.g.onedark_config.transparent
-  require('onedark').setup({ transparent = not transp })
-  require('onedark').load()
+  local cur_theme = vim.g.colors_name
+  local transp = true
+  if cur_theme == "onedark" then
+    transp = vim.g.onedark_config.transparent
+    require('onedark').setup({ transparent = not transp })
+    require('onedark').load()
+  elseif cur_theme == "catppuccin-mocha" then 
+    transp = require("catppuccin").options.transparent_background
+    require("catppuccin").setup({ transparent_background = not transp })
+    require("catppuccin").load()
+  else
+    print("Error: Not onedark or catppuccin-mocha.")
+  end
 end, { desc = '[Z]en Mode Toggle' })
 
